d02d10b9d29e13c27da3fdc599bcd11632e0e1fb
[Chukwuma Obasi] <59647298+Chubie@users.noreply.github.com>
2021-02-10 22:00:08 +0100

    Added my_any? and my_none

--- app.rb
+++ app.rb
@@ -56,12 +56,64 @@ module Enumerable
   end
 
   def my_any?
+    array = self
+
+    if block_given?
+      for number in array
+        result = yield number
+        return true if result
+      end
+    else
+      for number in array
+        return true if number
+      end
+    end
+    false
+  end
+
+
+def my_none?
+  array = self
+
+
+  if block_given?
+    for number in array
+      predicate = yield number
+      return false unless predicate
+    end
+  else
+    for number in array
+      return false unless number
+    end
+  end
+
+  true
+end
+
+  def my_count
+    array = self
+    array_length = 0
+    
+  
+    for item in array
+      array_length += 1
+      return array_length
+    end
+
+  elsif block_given?
+    for item in array.any?
+      array_length += 1
+    end
+
+
 
   end
+
 
 end
 
 
-p [].all? { |number| number.negative? }
+p [1,5,3,7,6,8,9].count { |number| number}
 
-p [].my_all? { |number| number.negative? }
+p [1,5,3,7,6,8,9].my_count { |number| number}
+
